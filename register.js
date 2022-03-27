54c6da545699b2cd0bfb8f50d39fec48a8c949ce
Gabriel <gabrielgoncalvesmachado@hotmail.com>
2022-03-26 23:41:53 -0300

    Adding RegEx validation for password

---
+++ register.js
@@ -0,0 +1,60 @@
+const numbers = /[0-9]/
+const minLenght = /[a-zA-Z0-9]{6}/
+const lowerCharacters = /[a-z]/
+const upperCharacters = /[A-Z]/
+const specialCharacters = /['"!@#$%¨&*()/;\.,<>º{}|\\]/
+
+const inputPassword = document.getElementById('inputPassword')
+const buttonRegister = document.getElementById('buttonRegister')
+const registerConfirmation = document.getElementById('registerConfirmation')
+
+function handleValidatePassword() {
+  if (!numbers.exec(inputPassword.value)) {
+    alert('A sua senha precisa ter números')
+
+    return false
+  }
+
+  if (!minLenght.exec(inputPassword.value)) {
+    alert('A sua senha precisa ter pelo menos 6 caracteres')
+
+    return false
+  }
+
+  if (!lowerCharacters.exec(inputPassword.value)) {
+    alert('A sua senha precisa ter caracteres minúsculos')
+
+    return false
+  }
+
+  if (!upperCharacters.exec(inputPassword.value)) {
+    alert('A sua senha precisa ter caracteres maiúsculos')
+
+    return false
+  }
+
+  if (!specialCharacters.exec(inputPassword.value)) {
+    alert('A sua senha precisa ter caracteres especiais')
+
+    return false
+  }
+
+  return true
+}
+
+function confirmRegister(e) {
+  if (handleValidatePassword()) {
+    registerConfirmation.innerHTML += '<p>Sucesso!</p>'
+  }
+
+  e.preventDefault()
+}
+
+if (
+  numbers &&
+  minLenght &&
+  lowerCharacters &&
+  upperCharacters &&
+  specialCharacters
+) {
+}

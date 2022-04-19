956643da0e715d5c9eae9577abaa583c7e45221b
GuiOkada <gui-okada@outlook.com>
2022-04-19 16:22:45 -0300

    Adding e-mail sending functions

--- php/email.php
+++ php/email.php
@@ -5,22 +5,42 @@
     require 'PHPMailer-master/src/PHPMailer.php';
     require 'PHPMailer-master/src/SMTP.php';
     $mail = new PHPMailer();
+
     // Configuração
     $mail->IsSMTP();
-    $mail->Mailer = "smtp";
-    $mail->IsSMTP(); 
+    $mail->Mailer = "smtp"; 
     $mail->CharSet = 'UTF-8';   
     $mail->SMTPDebug = 0;
     $mail->SMTPAuth = true;     
-    $mail->SMTPSecure = 'ssl'; 
+    $mail->SMTPSecure = 'tls'; 
     $mail->Host = 'smtp.gmail.com'; 
     $mail->Port = 465;
+
     // Detalhes do envio de E-mail
     $mail->Username = 'email'; 
     $mail->Password = 'senha';
     $mail->SetFrom('okada.gui@gmail.com', 'Guilherme');
+    $mail->addReplyTo('no-reply@email.com.br');
     $mail->addAddress('gaguigu321@gmail.com','Gabriel');
-    $mail->Subject = 'Test';
-    $mail->msgHTML('Teste');
-    $mail->send();
+    $mail->isHTML(true);
+    $mail->Subject = 'Confirmação de cadastro';
+    $mail->msgHTML = '<b>HTML! {Nome da pessoa}, obrigado(a) por se cadastrar em nosso site.
+
+    Para receber acesso a todo o conteúdo de estágios, por favor confirme o seu endereço de email clicando no link abaixo.
+    
+    Confirmar meu email
+    
+    Obrigado(a) por se inscrever,
+    
+    {Seu nome/Nome do seu negócio} </b>';
+
+    $mail->AltBody = 'Para visualizar essa mensagem acesse http://site.com.br/mail';
+    $mail->addAttachment('/tmp/image.jpg', 'nome.jpg'); //arquivos em anexo
+
+    if(!$mail->send()) {
+        echo 'Não foi possível enviar a mensagem.<br>';
+        echo 'Erro: ' . $mail->ErrorInfo;
+    } else {
+        echo 'Mensagem enviada.';
+    }
 ?>

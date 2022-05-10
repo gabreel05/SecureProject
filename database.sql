e548c891854d89b02e901974b8645a87538db06e
Gabriel Gonçalves Machado <gabriel.machado@compasso.com.br>
2022-05-10 16:29:19 -0300

    Modifying script.sql

--- database.sql
+++ database.sql
@@ -1,10 +1,8 @@
-CREATE DATABASE DB_SecureProject;
-
-DROP DATABASE DB_SecureProject;
+CREATE DATABASE IF NOT EXISTS DB_SecureProject;
+DROP DATABASE IF EXISTS DB_SecureProject;
 USE DB_SecureProject;
 
-DROP TABLE TB_Users;
-CREATE TABLE TB_Users(
+CREATE TABLE IF NOT EXISTS TB_Users(
 	user_id INTEGER PRIMARY KEY AUTO_INCREMENT,
     user_name VARCHAR(255) NOT NULL,
     user_email VARCHAR(255) NOT NULL,
@@ -14,6 +12,7 @@ CREATE TABLE TB_Users(
     user_country VARCHAR(255) NOT NULL,
     CONSTRAINT check_gender CHECK (user_gender = "Masculino" OR user_gender = "Feminino")
 );
+DROP TABLE IF EXISTS TB_Users;
 
 INSERT INTO TB_Users (user_name, user_email, user_phone, user_gender, user_password, user_country) 
 	VALUES ("Gabriel", "email@email", "(99) 99999-9999", "Masculino", "Gabriel@12345", "Brasil");

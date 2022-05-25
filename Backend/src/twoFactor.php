3c5a53694aafb5b6b7f4fef6542de88f38e4dea0
Gabriel <gabrielgoncalvesmachado@hotmail.com>
2022-05-24 22:04:03 -0300

    Adding OO

--- Backend/src/twoFactor.php
+++ Backend/src/twoFactor.php
@@ -10,26 +10,33 @@
     $user = $row[2];
   }
 
+  $code = rand(100000, 999999);
+
   $body = '
-    <tr>
-      <p>
-        <font size="3" face="Verdana">
-        Olá, [NAME]! 
-      </p>
-      </tr>
-      <tr>
-      <p>
-        <font size="3" face="Verdana">
-        Palestrinhas agradece pelo seu login.
-      </p>
-      <p>
-        <font size="3" face="Verdana">
-        <a href="http://localhost/SecureProject/Frontend/Modules/Loading/loading.html">Para concluir o login, clicke aqui</a>
-      </p>
-    </tr>
+<tr>
+  <p>
+    <font size="3" face="Verdana">
+    Olá, [NAME]! 
+  </p>
+</tr>
+<tr>
+  <p>
+    <font size="3" face="Verdana">
+    Palestrinhas agradece pelo seu login.
+  </p>
+  <p>
+    <font size="3" face="Verdana">
+    [NUMBERS]
+  </p>
+  <p>
+    <font size="3" face="Verdana">
+    Para concluir insira esta sequencia numerica para concluir o login
+  </p>
+</tr>
   ';
 
     $body = str_replace('[NAME]', $user, $body);
+    $body = str_replace('[BODY]', $code, $body);
 
     sendEmail($email, $user, 'Código de confirmação', $body);
 ?>

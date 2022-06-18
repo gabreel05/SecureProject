29b66d3c4412f2f278edf261393aabcc4e9e5b85
Gabriel <gabrielgoncalvesmachado@hotmail.com>
2022-06-18 19:44:35 -0300

    Refactoring home visualization of vacancies

--- Frontend/Modules/Home/home.js
+++ Frontend/Modules/Home/home.js
@@ -43,39 +43,41 @@ $(document).ready(function () {
     }
   })
 
-  $.ajax({
-    type: 'GET',
-    dataType: 'JSON',
-    url: '../../../Backend/src/getRecommendedVacancies.php',
-    success: function (data) {
-      for (let i = 0; i < data.length; i++) {
-        const cardRecommendedVacancyContent =
-          `<h5 class='card-title vacancyBrand'>${data[i].vacancy_brand}</h5>` +
-          `<p class='card-text vacancyDescription'>${data[i].vacancy_description}</p>` +
-          `<button href='#' class='btn btn-outline-secondary' id='registerForRecommendedVacancy${i}'>Se inscrever para vaga</button>`
+  $('#recommendedVacanciesLink').click(function () {
+    $.ajax({
+      type: 'GET',
+      dataType: 'JSON',
+      url: '../../../Backend/src/getRecommendedVacancies.php',
+      success: function (data) {
+        $('#cardVacancy').html('')
+        $('#cardTitle').html('Vagas recomendadas')
+        for (let i = 0; i < data.length; i++) {
+          const cardVacancyContent =
+            `<h5 class='card-title vacancyBrand'>${data[i].vacancy_brand}</h5>` +
+            `<p class='card-text vacancyDescription'>${data[i].vacancy_description}</p>` +
+            `<button type='button' class='btn btn-outline-secondary' id='registerForVacancy${i}'>Se inscrever para vaga</button>`
 
-        $('#cardRecommendedVacancy').append(cardRecommendedVacancyContent)
+          $('#cardVacancy').append(cardVacancyContent)
 
-        $(`#registerForRecommendedVacancy${i}`).click(function () {
-          $.ajax({
-            type: 'POST',
-            dataType: 'JSON',
-            url: '../../../Backend/src/registerForVacancy.php',
-            data: {
-              vacancy: data[i].vacancy_id
-            },
-            success: function (data) {
-              if (data === 'Data inserted successfuly') {
-                $(`#registerForRecommendedVacancy${i}`).text(
-                  'Inscrito com sucesso'
-                )
-                $(`#registerForRecommendedVacancy${i}`).prop('disabled', true)
+          $(`#registerForVacancy${i}`).click(function () {
+            $.ajax({
+              type: 'POST',
+              dataType: 'JSON',
+              url: '../../../Backend/src/registerForVacancy.php',
+              data: {
+                vacancy: data[i].vacancy_id
+              },
+              success: function (data) {
+                if (data === 'Data inserted successfuly') {
+                  $(`#registerForVacancy${i}`).text('Inscrito com sucesso')
+                  $(`#registerForVacancy${i}`).prop('disabled', true)
+                }
               }
-            }
+            })
           })
-        })
+        }
       }
-    }
+    })
   })
 
   $('#buttonLogout').click(function () {
